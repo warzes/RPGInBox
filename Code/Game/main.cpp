9716b53dd9aa3152c4c2bd553ac0998a3f36ca8a
warzes <warzes@mail.ru>
2021-08-09 14:26:52 +1000

    - фикс к предыдущему коммиту

--- Code/Game/main.cpp
+++ Code/Game/main.cpp
@@ -13,38 +13,44 @@
 //-----------------------------------------------------------------------------
 void GameMain() noexcept
 {
-	EngineConfig engineConfig;
-	engineConfig.window.width = 1024;
-	engineConfig.window.height = 768;
-	engineConfig.window.vsync = true;	
-
-	Engine engine;
-	if (engine.Init(engineConfig))
-	{
-		//OldGame game(engine);
-		Game game(engine);
-		if (game.Init())
-		{
-			while (!engine.IsEnd() && !game.IsEnd())
-			{
-				engine.Update();
-				game.Update(engine.GetDeltaTime());
-				game.Frame();
-			}
-		}
-	}
-}
-//-----------------------------------------------------------------------------
-int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
-{
-#if SE_DEBUG
+#if SE_DEBUG && SE_COMPILER_MSVC && SE_PLATFORM_WINDOWS
 	int flag = _CrtSetDbgFlag(_CRTDBG_REPORT_FLAG); // Get current flag
 	flag |= _CRTDBG_LEAK_CHECK_DF;   // Turn on leak-checking bit
 	flag |= _CRTDBG_CHECK_ALWAYS_DF; // Turn on CrtCheckMemory
 	_CrtSetDbgFlag(flag); // Set flag to the new value
 #endif
-	GameMain();
+
+	{
+		EngineConfig engineConfig;
+		engineConfig.window.width = 1024;
+		engineConfig.window.height = 768;
+		engineConfig.window.vsync = true;
+
+		Engine engine;
+		if (engine.Init(engineConfig))
+		{
+			//OldGame game(engine);
+			Game game(engine);
+			if (game.Init())
+			{
+				while (!engine.IsEnd() && !game.IsEnd())
+				{
+					engine.Update();
+					game.Update(engine.GetDeltaTime());
+					game.Frame();
+				}
+			}
+		}
+	}
+
+#if SE_DEBUG && SE_COMPILER_MSVC && SE_PLATFORM_WINDOWS
 	_CrtDumpMemoryLeaks();
+#endif
+}
+//-----------------------------------------------------------------------------
+int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[])
+{
+	GameMain();
 	return 0;
 }
 //-----------------------------------------------------------------------------
