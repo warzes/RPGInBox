055544b1b9aa74eaa5a99486ecd083b19459c318
warzes <warzes@mail.ru>
2021-09-20 21:07:40 +1000

    - новый тест боевой системы

--- src/Game/Engine.h
+++ src/Game/Engine.h
@@ -2,20 +2,18 @@
 
 #include "Log.h"
 
-struct WindowConfig final
-{
-	int width = 1280;
-	int height = 768;
-	bool fullscreen = false;
-	bool resizable = true;
-	bool vsync = true;
-};
-
 struct EngineConfig final
 {
 	const char* LogName = "Log.txt";
 
-	WindowConfig window;
+	struct
+	{
+		int width = 1280;
+		int height = 768;
+		bool fullscreen = false;
+		bool resizable = true;
+		bool vsync = true;
+	} window;
 };
 
 class Engine final : NonCopyable, NonMovable
