aa66364bd9b395dc52007a3a756addad298cfbdd
warzes <warzes@mail.ru>
2021-10-04 22:54:16 +1000

    - ссылки

--- src/Game/Creature.h
+++ src/Game/Creature.h
@@ -1,1 +1,19 @@
-#pragma once
+﻿#pragma once
+
+// сила, ловкость, выносливость, интеллект
+struct Attributes
+{
+
+};
+
+// очки здоровья, магии, защита и т.
+struct Stats
+{
+
+};
+
+
+https://gamefaqs.gamespot.com/pc/564785-pool-of-radiance/faqs/73869
+http://shrines.rpgclassics.com/nes/ff1/whitemagic.shtml
+http://shrines.rpgclassics.com/pc/mm2/characters.shtml
+http://shrines.rpgclassics.com/pc/
